a3cc2c496541446fd1fa5459025e00f109ebe9e5
kenza1525 <kerianenzabampema@gmail.com>
2022-09-20 13:20:47 -0700

    added 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+def uppercase(str):
+    for i in str:
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            ord(str[i]) = ord(str[i]) - 32
+            print("{:c}".format(ord(str[i])), end='')
+    return(str)
