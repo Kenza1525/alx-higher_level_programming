b458cc0f88706b938653ca72b991c50fb597f33a
kenza1525 <kerianenzabampema@gmail.com>
2022-09-22 12:19:55 -0700

    Added 100-my_calculator file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     num_arg = len(argv[1:])
     if num_arg != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>"i)
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     sign = ['+', '-', '*', '/']
     if argv[2] not in sign:
@@ -20,4 +20,4 @@ if __name__ == "__main__":
     if op == "*":
         print("{:d} * {:d} = {:d}".foramt(num1, num2, mul(num1, num2)))
     if op == "/":
-        print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
+        print("{} / {} = {}".format(num1, num2, div(num1, num2)))
