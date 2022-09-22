7611e3729fee0e9f9d44ac600232bcc985425f5b
kenza1525 <kerianenzabampema@gmail.com>
2022-09-22 07:52:07 -0700

    Added 2-args file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,12 @@ if __name__ == "__main__":
     num_arg = len(argv[1:])
     if num_arg == 0:
         print("{} arguments.".format(num_arg))
+    elif num_arg == 1:
+        print("{} argument:".format(num_arg))
+        print("{}: {}".format(num_arg, argv[1]))
     else:
         print("{} arguments:".format(num_arg))
         i = 1
         while(num_arg >= i):
-            print("{} : {}".format(i, argv[i]))
+            print("{}: {}".format(i, argv[i]))
             i += 1
