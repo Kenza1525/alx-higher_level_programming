a8cf7fb2f98df85980e36b02f7e5ff7d551f1816
kenza1525 <kerianenzabampema@gmail.com>
2022-09-26 12:42:30 -0700

    Added 100-print_python_list_info.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * is_palindrome - check if a list is palindrome
+ * @head: head of the list
+ * Return: 0 if is not palindrome or 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int size = 0, idx = 0;
+	int buffer[2043];
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (*head != NULL)
+	{
+		buffer[size] = (*head)->n;
+		size++;
+		(*head) = (*head)->next;
+	}
+
+	while (size >= idx)
+	{
+		if (buffer[idx] != buffer[size - 1])
+			return (0);
+		size--;
+		idx++;
+	}
+	return (1);
+
+
+}
