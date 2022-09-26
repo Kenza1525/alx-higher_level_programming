03a1b4c76c6aa335195558f2bf0162ae3a7e7cfd
kenza1525 <kerianenzabampema@gmail.com>
2022-09-26 05:15:28 -0700

    Added a readme ND 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    i = len(my_list) - 1
     for i in my_list:
-        print("{}".format(i))
+        print("{}".format(my_list[i]))
+    return 0
