f4e4f02084375a929ad49776445033e1b84d197b
kenza1525 <kerianenzabampema@gmail.com>
2022-10-19 13:20:24 -0700

    Fixed some errors in 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,93 +1,144 @@
 #!/usr/bin/python3
-"""create a Rectangle class with widht and height attribute"""
+"""101-nqueens finds all possible solutions the N queens puzzle, including
+translations and reflections.
+Attempted virtual backtracking without recursion. In local tests process will
+start to slow down visibly for N > 8, and is successful up to N = 11 but
+will be killed if used for N > 11. Recursion could allow for a lighter weight
+process, but it's not yet apparent to this student how to retain a record of
+which solutions are already derived with that method.
+Attributes:
+    N (int): base number of queens, and length of board side in piece positions
+    candidates (list) of (list) of (list) of (int): list of all successful
+        solutions for given amount of columns checked
+"""
+from sys import argv
+
+if len(argv) is not 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+if not argv[1].isdigit():
+    print('N must be a number')
+    exit(1)
+
+N = int(argv[1])
+
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
 
 
-class Rectangle:
-    """Rectangle class created with width and height
-    Attributes:
-        number_of_instances (int): shows the number of instances created
-        and deleted
-        print_symbo (str): characters to print
+def board_column_gen(board=[]):
+    """Adds a column of zeroes to the right of any board about to be tested for
+    queen arrangements in that column.
+    Args:
+        board (list) of (list) of (int): 2D list of ints, only as wide as
+        needed to test the rightmost column for queen conflicts.
+    Returns:
+        modified 2D list
     """
-    number_of_instances = 0
-    print_symbol = "#"
-    """create a functions for ractengle"""
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
-        type(self).number_of_instances += 1
+    if len(board):
+        for row in board:
+            row.append(0)
+    else:
+        for row in range(N):
+            board.append([0])
+    return board
 
-    @property
-    def height(self):
-        """get height"""
-        return self.__height
 
-    @height.setter
-    def height(self, height):
-        """set height"""
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = height
+def add_queen(board, row, col):
+    """Sets "queen," or 1, to coordinates given in board.
+    Args:
+        board (list) of (list) of (int): 2D list of ints, only as wide as
+            needed to test the rightmost column for queen conflicts.
+        row (int): first dimension index
+        col (int): second dimension index
+    """
+    board[row][col] = 1
 
-    @property
-    def width(self):
-        """get width"""
-        return self.__width
 
-    @width.setter
-    def width(self, width):
-        """set width"""
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = width
+def new_queen_safe(board, row, col):
+    """For the board given, checks that for a new queen placed in the rightmost
+    column, there are no other "queen"s, or 1 values, in the martix to the
+    left, and diagonally up-left and down-left.
+    Args:
+        board (list) of (list) of (int): 2D list of ints, only as wide as
+            needed to test the rightmost column for queen conflicts.
+        row (int): first dimension index
+        col (int): second dimension index
+    Returns:
+        True if no left side conflicts found for new queen, or False if a
+    conflict is found.
+    """
+    x = row
+    y = col
 
-    def area(self):
-        """return the area of a rectangle"""
-        return self.height * self.width
+    for i in range(1, N):
+        if (y - i) >= 0:
+            # check up-left diagonal
+            if (x - i) >= 0:
+                if board[x - i][y - i]:
+                    return False
+            # check left
+            if board[x][y - i]:
+                return False
+            # check down-left diagonal
+            if (x + i) < N:
+                if board[x + i][y - i]:
+                    return False
+    return True
 
-    def perimeter(self):
-        """return perimeter of a rectangle"""
-        if self.height == 0 or self.width == 0:
-            return 0
-        return 2 * (self.height + self.width)
 
-    def __str__(self):
-        """print a rectangle"""
-        pr = []
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        for i in range(self.height):
-            for j in range(self.width):
-                pr.append(str(self.print_symbol))
-            if i < self.height - 1:
-                pr.append("\n")
-        return "".join(pr)
+def coordinate_format(candidates):
+    """Converts a board (matrix of 1 and 0) into a series of row/column
+    indicies of each queen/1.
+    Args:
+    candidates (list) of (list) of (list) of (int): list of all successful
+        solutions for amount of columns last checked
+    Attributes:
+        holberton (list) of (list) of (int): each member list contains the row
+    column number for each queen found
+    Returns:
+        holberton, the list of coordinates
+    """
+    holberton = []
+    for x, attempt in enumerate(candidates):
+        holberton.append([])
+        for i, row in enumerate(attempt):
+            holberton[x].append([])
+            for j, col in enumerate(row):
+                if col:
+                    holberton[x][i].append(i)
+                    holberton[x][i].append(j)
+    return holberton
 
-    def __repr__(self):
-        """return the rectangle width and height"""
-        return f"Rectangle ({self.__width}, {self.__height})"
+# init candidates list with first column of 0s
+candidates = []
+candidates.append(board_column_gen())
 
-    def __del__(self):
-        """delete a instance"""
-        type(self).number_of_instances -= 1
-        print("Bye rectangle...")
+# proceed column by column, testing the rightmost
+for col in range(N):
+    # start a new generation of the candidate list for every round of testing
+    new_candidates = []
+    # test each candidate from previous round, at current column
+    for matrix in candidates:
+        # for every row in that candidate's rightmost column
+        for row in range(N):
+            # are there any conflicts in placing a queen at those coordinates?
+            if new_queen_safe(matrix, row, col):
+                # no? then create a "child" (copy) of that candidate
+                temp = [line[:] for line in matrix]
+                # place a queen in that position
+                add_queen(temp, row, col)
+                # and unless you're in the last round of testing,
+                if col < N - 1:
+                    # add a new column of 0s on the right for the next round
+                    board_column_gen(temp)
+                # add that new candidate to this round's list of successes
+                new_candidates.append(temp)
+    # when finished with the round, discard the "parent" candidates
+    candidates = new_candidates
 
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if Rectangle.area(rect_1) < Rectangle.area(rect_2):
-            return rect_2
-        return rect_1
-
-    @classmethod
-    def square(cls, size=0):
-        """Returns a new Rectangle instance"""
-
-        return cls(size, size)
+# format results to match assignment output
+for item in coordinate_format(candidates):
+    print(item)
