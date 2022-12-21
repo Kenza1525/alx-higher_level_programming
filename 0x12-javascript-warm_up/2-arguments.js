fd6da2af83aecc127371ce765d901a333d4d42a9
Kenza1525 <kerianenzabampema@gmail.com>
2022-12-21 11:06:36 +0000

    removed the variables in 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 
-const process = require('process');
-let args = process.argv;
-
-if (args.length < 3)
+if (process.argv.length < 3)
 {
 	console.log('No argument');
 }
-else if (args.length === 3)
+else if (process.argv.length === 3)
 {
 	console.log('Argument found');
 }
