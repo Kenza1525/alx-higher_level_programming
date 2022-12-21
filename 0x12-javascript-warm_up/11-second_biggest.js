6bc14ea1708b72049c4b9e451bef783cd3ad72fa
Kenza1525 <kerianenzabampema@gmail.com>
2022-12-21 18:33:55 +0200

    used push and sort functions

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,11 +3,12 @@
 const args = process.argv;
 const num = process.argv[2];
 const arg3 = process.argv[3];
+const arr = [];
 if (!num || !arg3) {
   console.log('0');
 }
 for (let i = 2; i < args.length; i++) {
-  if (args[i] > num) {
-    console.log(num);
-  }
+  arr.push(parseInt(args[i]));
 }
+arr.sort(function (a, b) { return a - b; });
+console.log(arr[arr.length - 2]);
