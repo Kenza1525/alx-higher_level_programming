5b7e847ce93d81dbc2f3b0d2cb069b42311d46a0
Kenza1525 <kerianenzabampema@gmail.com>
2022-12-21 16:40:19 +0200

    Removed spaces at the end of file

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 
-function add(a, b) {
+function add (a, b) {
   if (process.argv.length < 3) {
     console.log('NaN');
   } else {
@@ -11,4 +11,3 @@ function add(a, b) {
   }
 }
 add(a, b);
-
