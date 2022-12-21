3570e83bc5b629a05c27720a48ec3464fdd7f655
Kenza1525 <kerianenzabampema@gmail.com>
2022-12-21 16:36:52 +0200

    Added 9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add(a, b) {
+  if (process.argv.length < 3) {
+    console.log('NaN');
+  } else {
+    console.log(a + b);
+  }
+}
+add(a, b);
+
