b83669eec4afa1e1cd4a7d6af989c7b70c57e9b7
Kenza1525 <kerianenzabampema@gmail.com>
2022-12-21 11:01:52 +0000

    added 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const process = require('process');
+let args = process.argv;
+
+if (args.length < 3)
+{
+	console.log('No argument');
+}
+else if (args.length === 3)
+{
+	console.log('Argument found');
+}
+else
+{
+	console.log('Arguments found');
+}
