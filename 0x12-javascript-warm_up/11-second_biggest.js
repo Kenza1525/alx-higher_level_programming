ec2ce2401c3d30d3e7def220d9f4606a5b15ea98
Kenza1525 <kerianenzabampema@gmail.com>
2022-12-21 18:36:58 +0200

    Added an else condition

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,9 +6,10 @@ const arg3 = process.argv[3];
 const arr = [];
 if (!num || !arg3) {
   console.log('0');
+} else {
+  for (let i = 2; i < args.length; i++) {
+    arr.push(parseInt(args[i]));
+  }
+  arr.sort(function (a, b) { return a - b; });
+  console.log(arr[arr.length - 2]);
 }
-for (let i = 2; i < args.length; i++) {
-  arr.push(parseInt(args[i]));
-}
-arr.sort(function (a, b) { return a - b; });
-console.log(arr[arr.length - 2]);
