61b53710876c6753736a3522b493b3af52375a45
Kenza1525 <kerianenzabampema@gmail.com>
2023-01-06 13:02:13 +0200

    Modifird 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ starting with N from the database hbtn_0e_0_usa """
 
 import MySQLdb
 from sys import argv
-f __name__ == "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(
          host='localhost',
          port=3306,
