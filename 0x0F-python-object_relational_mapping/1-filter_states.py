11643ac2be791620d911c23cf0298063a44d7efa
Kenza1525 <kerianenzabampema@gmail.com>
2023-01-06 13:00:11 +0200

    Modified 1-filter_statespy

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,16 +5,16 @@ starting with N from the database hbtn_0e_0_usa """
 
 import MySQLdb
 from sys import argv
+f __name__ == "__main__":
+    db = MySQLdb.connect(
+         host='localhost',
+         port=3306,
+         user=argv[1],
+         passwd=argv[2],
+         db=argv[3])
 
-db = MySQLdb.connect(
-     host='localhost',
-     port=3306,
-     user=argv[1],
-     passwd=argv[2],
-     db=argv[3])
-
-cur = db.cursor()
-cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY states.id ASC')
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY states.id ASC')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
