bffb63dd6779cce9340d2b89a6409c935f34998c
kenza1525 <kerianenzabampema@gmail.com>
2023-01-16 13:20:28 -0800

    Changed d option to F

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a GET request to the URL, and displays the body of the response.
-curl -s -X POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
+curl -s -X POST -F "email=hr@holbertonschool.com" -F "subject=I will always be here for PLD" "$1"
