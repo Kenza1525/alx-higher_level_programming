03b728c1393972568a3014252b318a03dab5dfa3
kenza1525 <kerianenzabampema@gmail.com>
2023-01-24 11:58:03 -0800

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" class Square that defines a square by size"""
+
+
+class Square:
+    """ Content of the class """
+
+    def __init__(self, size):
+        """ Instance method that initialize the object """
+
+        self.__size = size
