2066e7cf94f4f8d948db339c89a98358c6dea3a2
kenza1525 <kerianenzabampema@gmail.com>
2023-01-24 11:34:23 -0800

    Removed 0x06-python-classes directory

--- 0x06-python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Creating an empty Class"""
-
-
-class Square:
-    """pass to executes empty Class"""
-    pass
--- 0x06-python-classes/1-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Square create size value"""
-
-
-class Square:
-    """create size value"""
-    def __init__(self, size):
-        self.__size = size
