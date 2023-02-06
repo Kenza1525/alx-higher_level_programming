49209f391d3c637154739acd0f76312c132793f2
kenza1525 <kerianenzabampema@gmail.com>
2023-02-06 12:03:30 -0800

    Removed trailling space

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" eturns True if the object is exactly an instance of the specified class """
+"""a function that returns True if the
+object is exactly an instance of the specified class"""
 
 
 def is_same_class(obj, a_class):
-    """ Check if an object is an instance of a class """
-        return type(obj) is a_class
+    """return true or false"""
+    return type(obj) is a_class
