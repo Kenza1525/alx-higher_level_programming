ee074b7d9fd71859362c003f707492a02cd03fda
kenza1525 <kerianenzabampema@gmail.com>
2023-02-06 12:24:15 -0800

    Added 4-inherits_from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Returns True or False based on if
+the object is an instance of a class"""
+
+
+def inherits_from(obj, a_class):
+    """Return True or False"""
+    return isinstance(obj, a_class) and type(obj) != a_class 
