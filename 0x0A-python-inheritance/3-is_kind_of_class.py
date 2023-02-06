ed87cf298f647022f25da55b972727aaaf2ef652
kenza1525 <kerianenzabampema@gmail.com>
2023-02-06 12:14:58 -0800

    Added 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""""check if a variable is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if a variable is an instance of a class"""
+    return isinstance(obj, a_class)
