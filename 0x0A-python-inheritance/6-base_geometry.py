8478ec1ed4b7e4b4fdf2fc0dcb4835ed0189afc2
kenza1525 <kerianenzabampema@gmail.com>
2023-02-06 12:32:33 -0800

    Added 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""define BaseGeometry"""
+
+
+class BaseGeometry:
+    """Area function that raise an error"""
+
+    def area(self):
+        """Raise an exception error"""
+
+        raise Exception("area() is not implemented")
