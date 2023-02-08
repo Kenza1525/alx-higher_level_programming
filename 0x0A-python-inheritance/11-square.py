ff537ea7393b2aa6e9ccf5425b2a8ee6b51e6b01
kenza1525 <kerianenzabampema@gmail.com>
2023-02-08 01:59:20 -0800

    Added 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" class Square that inherits from Rectangle """
+Rectangle = __import__("9-rectangle").Rectangle
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Square(Rectangle):
+    """ set the size value and calculate the Square area """
+
+    def __init__(self, size):
+        """ Initialize the size attribute """
+        self.__size = size
+        BaseGeometry.integer_validator(self, "size", self.__size)
+
+    def area(self):
+        """ Calculates and returns area """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ Prints the name of the class in string format """
+        name = Square.__name__
+        return f"[{name}] {self.__size}/{self.__size}"
