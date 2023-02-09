71c9171942574df03aaf951ff07a8b760447a269
kenza1525 <kerianenzabampema@gmail.com>
2023-02-09 07:39:17 -0800

    Removed trailing space

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ function that returns the JSON representation
 of an object (string) """
-
-
-
 import json
+
 
 def to_json_string(my_obj):
     """ Uses the dumps() method for derialization """
