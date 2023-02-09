61b2d3e6a081aeecc5a385473d77bf7d13c6f9d0
kenza1525 <kerianenzabampema@gmail.com>
2023-02-09 06:12:59 -0800

    Added 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function that reads a text file (UTF8) and prints it to stdout """
+
+
+def read_file(filename=""):
+    """ Reads a file and prints it """
+    with open("my_file_0.txt", encoding="utf-8") as f:
+        filename = f.read()
+        print(filename.rstrip())
