20be4721399820fdde6cbd78f2e086d9e4584af6
kenza1525 <kerianenzabampema@gmail.com>
2023-04-19 11:46:34 -0700

    addde if __name__ == '__main__'

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,17 +6,19 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
 
-user = argv[1]
-passwd = argv[2]
-db = argv[3]
-engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}')
+if __name__ == '__main__':
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine(f'mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}')
 
-Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
 
-session = sessionmaker(bind=engine)
-session = session()
+    session = sessionmaker(bind=engine)
+    session = session()
 
-rows = session.query(State).order_by(State.id).all()
+    rows = session.query(State).order_by(State.id).all()
 
-for row in rows:
-    print(f"{row.id}: {row.name}")
+    for row in rows:
+        print(f"{row.id}: {row.name}")
+    session.close()
