fd30cf293e714ddec4f5f27e832f80f8e622f2de
Keriane Nzabampema <kerianenzabampema@gmail.com>
2023-04-27 23:12:45 +0200

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a JSON POST request to a URL and displays the body response
-curl -sH "Content-Type: application/json" --request POST --data @"$2" "$1"
+curl -sH "Content-Type: application/json" -X POST -d @"$2" "$1"
