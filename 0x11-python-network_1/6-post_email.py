76548244e4c974e65b55dc5b75e2857ab1485af0
Kenza1525 <kerianenzabampema@gmail.com>
2023-04-28 10:34:16 -0700

    changed file permission

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""
-takes in a URL and an email, sends a POST request to the passed URL
-with the email as a parameter, and displays the body of the
-response (decoded in utf-8) using requests
-"""
+""" takes in a URL and an email address, sends a POST request to
+the passed URL with the email as a parameter
+and finally displays the body of the response """
+
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    """
-    takes in a URL and an email, sends a POST request to the passed
-    URL with the email as a parameter, and displays the body of the
-    response (decoded in utf-8) using requests
-    """
     url = argv[1]
-    r = requests.post(url, data={'email': argv[2]})
-    print(r.text)
+    email = {"email": argv[2]}
+    response = requests.post(url, email)
+    print(response.text)
