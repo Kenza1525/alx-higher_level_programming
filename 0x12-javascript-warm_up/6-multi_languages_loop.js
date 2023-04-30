a9103a3c7425fff9ab003f332d991f2d9625a8ea
Kenza1525 <kerianenzabampema@gmail.com>
2023-04-30 18:53:27 +0000

    Added 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arrayStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrayLength = arrayStrings.length;
+let i = 0;
+
+while (i < arrayLength) {
+  console.log(arrayStrings[i]);
+  i++;
+}
