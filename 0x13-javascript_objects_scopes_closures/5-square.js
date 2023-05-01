4ce3f44aac969142591f57af8f280f3c73c8970a
Kenza1525 <kerianenzabampema@gmail.com>
2023-05-01 18:17:55 +0000

    Added 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  print () {
+    super.print();
+  }
+
+  double () {
+    super.double();
+  }
+}
+
+module.exports = Square;
