33c411ddfdd468db5c35df3909d302f5d01498ec
Kenza1525 <kerianenzabampema@gmail.com>
2023-05-01 18:22:02 +0000

    Reduced number of lines

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -5,15 +5,6 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.size = size;
-  }
-
-  print () {
-    super.print();
-  }
-
-  double () {
-    super.double();
   }
 }
 
